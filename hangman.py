2fcbb83f0593728a60e9c9fcf3d9b7c18c195f88
kcheli <44278609+kcheli@users.noreply.github.com>
2021-05-18 18:22:05 -0400

    added control for empty space input

--- hangman.py
+++ hangman.py
@@ -8,7 +8,7 @@ palabra_sin_tildes = quitar_tildes(palabra)
 vidas = 10
 letras_usadas = set()
 respuesta = set()
-print(palabra)
+
 
 while respuesta != set(palabra_sin_tildes) and vidas > 0:
     print('Vidas:', vidas)
@@ -19,8 +19,8 @@ while respuesta != set(palabra_sin_tildes) and vidas > 0:
         else:
             palabra_escondida += "_"
         print(palabra_escondida, end=" ")
-    letra_usuario = input('\nIngresa una letra: ')
-    while len(letra_usuario) > 1:
+    letra_usuario = input('\nIngresa una letra: ').strip()
+    while len(letra_usuario) > 1 or letra_usuario == '':
         letra_usuario = input('\nUna sola letra, por favor: ')
     while letra_usuario not in string.ascii_lowercase:
         letra_usuario = input('\nNo es una letra: ')
@@ -35,9 +35,3 @@ if not vidas:
     print('Has perdido, la palabra era:', palabra.upper())
 else:
     print('\n****** Ganaste! La palabra es:', palabra.upper(), '******')
-
-
-
-
-
-
